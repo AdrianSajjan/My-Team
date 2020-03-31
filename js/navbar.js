8e26837f44e63c03b50e3093c960bca61884839d
AdrianSajjan <50446145+AdrianSajjan@users.noreply.github.com>
2020-03-31 19:41:19 +0530

    Navbar Animations Added

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,7 @@ $(() => {
   $targetNav = $(".nav-collapse").attr("data-target");
   $(".nav-collapse").click(() => {
     $(".nav-collapse").toggleClass("collapsed");
+    $(".nav-collapse").toggleClass("open");
     $($targetNav).toggleClass("show-nav");
   });
 });
