89b761b47d65cc37dc5486fce099ad3cc39c8729
AdrianSajjan <50446145+AdrianSajjan@users.noreply.github.com>
2020-04-01 19:49:34 +0530

    Added Form Validation

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,5 @@
+$emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 // prettier-ignore
 const CheckElementActive = () => {
   $(".form-input").each((index, element) => {
@@ -21,13 +23,55 @@ const CheckElementActive = () => {
 const HandleFromSubmit = () => {
   $("#form").submit(event => {
     event.preventDefault();
+
+    $emptyFields = false;
+
     $(".form-input").each((index, element) => {
-      //prettier-ignore
       if ($(element).val().length == 0) {
-        $(element).next().next().html("Please fill this field!");
-        return;
+        $(element)
+          .next()
+          .next()
+          .html("Please fill this field!");
+        $emptyFields = true;
       }
     });
+
+    if ($emptyFields) return;
+
+    if ($("#name-input").val().length < 5) {
+      $("#name-input")
+        .next()
+        .next()
+        .html("Please enter your full name!");
+      return;
+    }
+
+    if (!$emailRegex.test($("#email-input").val())) {
+      $("#email-input")
+        .next()
+        .next()
+        .html("Please enter a valid email!");
+      return;
+    }
+
+    if ($("#title-input").val().length < 10) {
+      $("#title-input")
+        .next()
+        .next()
+        .html("Title cannot be less than 10 letters!");
+      return;
+    }
+
+    if ($("#message-input").val().length < 20) {
+      $("#message-input")
+        .next()
+        .next()
+        .html("Please enter a message of more than 20 letters.");
+      return;
+    }
+
+    alert("Thank You for reaching to us!\nWe will get to you shortly!");
+    $("#form").trigger("reset");
   });
 };
 
