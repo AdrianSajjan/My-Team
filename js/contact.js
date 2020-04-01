7d7e0f270e447f3f9f41a1e3616d83bce3733da4
AdrianSajjan <50446145+AdrianSajjan@users.noreply.github.com>
2020-04-01 17:11:57 +0530

    Fine Tweaks

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,37 @@
-$(() => {
+// prettier-ignore
+const CheckElementActive = () => {
+  $(".form-input").each((index, element) => {
+    $(element).focusout(() => {
+
+      if ($(element).val().length > 0) {
+        $(element).next().next().html("");
+        if(!$(element).hasClass("form-input-active"))
+          $(element).addClass("form-input-active");
+      }
+
+      if ($(element).val().length == 0) {
+        if($(element).hasClass("form-input-active"))
+          $(element).removeClass("form-input-active");
+      }
+
+    });
+  });
+};
+
+const HandleFromSubmit = () => {
   $("#form").submit(event => {
     event.preventDefault();
-    console.log("Submit");
+    $(".form-input").each((index, element) => {
+      //prettier-ignore
+      if ($(element).val().length == 0) {
+        $(element).next().next().html("Please fill this field!");
+        return;
+      }
+    });
   });
+};
+
+$(() => {
+  CheckElementActive();
+  HandleFromSubmit();
 });
