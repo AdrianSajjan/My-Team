5d8f9d73e6a4053aaba5b07dac09bcbc5988e7eb
AdrianSajjan <50446145+AdrianSajjan@users.noreply.github.com>
2020-04-01 11:34:53 +0530

    Fixed Nav Collapse

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(window).on("load", () => {
-  $targetNav = $(".nav-collapse").attr("data-target");
-  $(".nav-collapse").click(() => {
-    $(".nav-collapse").toggleClass("collapsed");
-    $(".nav-collapse").toggleClass("open");
-    $($targetNav).toggleClass("show-nav");
-  });
-});
